788b738b11e2a982ad1d7329771090dd633577fb
Jansen Ducusin <senducusin@gmail.com>
2021-03-30 11:18:50 +0800

    Initial commit

---
+++ TTC-Routes/TTC-Routes/Controllers/RouteListController.swift
@@ -0,0 +1,12 @@
+//
+//  RouteListController.swift
+//  TTC-Routes
+//
+//  Created by Jansen Ducusin on 3/30/21.
+//
+
+import UIKit
+
+class RouteListController: UITableViewController {
+    
+}
---
+++ TTC-Routes/TTC-Routes/Services/WebService.swift
@@ -0,0 +1,16 @@
+//
+//  WebService.swift
+//  TTC-Routes
+//
+//  Created by Jansen Ducusin on 3/30/21.
+//
+
+import Foundation
+
+
+
+class WebService{
+    func load<T>(resource:Resource<T>, completion:@escaping(Result<T,NetworkError>)->()){
+                    
+    }
+}
